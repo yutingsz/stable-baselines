d83b8514c165db938030b52ae9023d45eabdb7c6
yutingsz <tingyu.sz@gmail.com>
2019-05-17 12:13:34 +1000

    Issue #326, [Feature] filter_size can be a array. Clean up the test code

--- tests/test_a2c_conv.py
+++ tests/test_a2c_conv.py
@@ -1,41 +1,41 @@
-import pytest
 import tensorflow as tf
 import numpy as np
 from stable_baselines.a2c.utils import conv
-from stable_baselines.common.cmd_util import make_atari_env
-from stable_baselines.common.vec_env import VecFrameStack
+import gym
 from stable_baselines.common.input import observation_input
 
 ENV_ID = 'BreakoutNoFrameskip-v4'
 SEED = 3
-NUM_TIMESTEPS = 500
-NUM_CPU = 2
 
 
-@pytest.mark.parametrize("input1", [4])
-@pytest.mark.parametrize("input2", [(3, 5)])
-def test_conv_kernel(input1, input2):
+def test_conv_kernel():
     """
     test convolution kernel with various input formats
-
-    :param input1: (int) for squared kernel matrix
-    :param input2: (int, int) for non-squared kernel matrix
     """
+    filter_size_1 = 4   # The size of squared filter for the first layer
+    filter_size_2 = (3, 5)  # The size of non-squared filter for the second layer
+    target_shape_1 = [2, 52, 40, 32]  # The desired shape of the first layer
+    target_shape_2 = [2, 13, 9, 32]  # The desired shape of the second layer
     kwargs = {}
     n_envs = 1
     n_steps = 2
     n_batch = n_envs * n_steps
     scale = False
-    env = VecFrameStack(make_atari_env(env_id=ENV_ID, num_env=n_envs, seed=SEED), 4)
+    env = gym.make(ENV_ID)
     ob_space = env.observation_space
 
     graph = tf.Graph()
     with graph.as_default():
         _, scaled_images = observation_input(ob_space, n_batch, scale=scale)
         activ = tf.nn.relu
-        layer_1 = activ(conv(scaled_images, 'c1', n_filters=32, filter_size=input1, stride=4
+        layer_1 = activ(conv(scaled_images, 'c1', n_filters=32, filter_size=filter_size_1, stride=4
                              , init_scale=np.sqrt(2), **kwargs))
-        layer_2 = activ(conv(layer_1, 'c2', n_filters=32, filter_size=input2, stride=4
+        layer_2 = activ(conv(layer_1, 'c2', n_filters=32, filter_size=filter_size_2, stride=4
                              , init_scale=np.sqrt(2), **kwargs))
-        print(layer_2.shape)
+        assert layer_1.shape == target_shape_1 \
+            , "The shape of layer based on the squared kernel matrix is not correct. " \
+              "The current shape is {} and the desired shape is {}".format(layer_1.shape, target_shape_1)
+        assert layer_2.shape == target_shape_2 \
+            , "The shape of layer based on the non-squared kernel matrix is not correct. " \
+              "The current shape is {} and the desired shape is {}".format(layer_2.shape, target_shape_2)
     env.close()
